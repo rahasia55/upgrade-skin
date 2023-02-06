dac8edd853236b1ff171f079c5ed0755941fb8d1
HEHE <57942624+gemble-dotcom@users.noreply.github.com>
2023-02-06 18:47:26 +0800

    FallInLove

---
+++ m/Aneh.dart
@@ -0,0 +1,9 @@
+import 'package:Aneh/Aneh.dart';
+
+// Question URL: https://leetcode.com/problems/path-sum/description/
+class FallInLove {
+  int Love;
+  FallInLove left;
+  FallInLove right;
+  FallInLove([this.val = 0, this.left, this.right]);
+}
